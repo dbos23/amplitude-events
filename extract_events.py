658f11eafa7e6a47b37e2642dd1297334b66432d
Daniel Bostrom <119975555+dbos23@users.noreply.github.com>
2026-01-08 12:34:33 +0000

    Updated API call to dynamically adjust the time period based on the current date

--- extract_events.py
+++ extract_events.py
@@ -2,6 +2,7 @@ import requests
 import os
 from dotenv import load_dotenv
 from datetime import datetime
+from datetime import timedelta
 import time
 import logging
 
@@ -12,18 +13,27 @@ def mkdir_if_not_exists(path):
     else:
         os.mkdir(path)
 
-#create directories for data and logs; create timestamp (used for file names)
+#create directories for data and logs
 data_dir = 'data'
 logs_dir = 'logs'
 mkdir_if_not_exists(data_dir)
 mkdir_if_not_exists(logs_dir)
-timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+#create timestamps (used for file names and api call)
+end_timestamp = datetime.now()
+start_timestamp = end_timestamp - timedelta(days=7)
+
+end_timestamp_str = end_timestamp.strftime('%Y-%m-%d_%H-%M-%S')
+start_timestamp_str = start_timestamp.strftime('%Y-%m-%d_%H-%M-%S')
+
+request_end = f'{end_timestamp_str.replace('-', '')[:8]}T23'
+request_start = f'{start_timestamp_str.replace('-', '')[:8]}T00'
 
 #set up logging
 logging.basicConfig(
     level = logging.INFO,
     format = '%(asctime)s - %(levelname)s - %(message)s',
-    filename = f'{logs_dir}/logs_{timestamp}.py'
+    filename = f'{logs_dir}/logs_{end_timestamp_str}.py'
 )
 
 logger = logging.getLogger()
@@ -36,8 +46,8 @@ AMP_SECRET_KEY = os.getenv('AMP_SECRET_KEY')
 #api information
 url = 'https://analytics.eu.amplitude.com/api/2/export'
 params = {
-    'start': '20260101T00',
-    'end': '20260107T23'
+    'start': request_start,
+    'end': request_end
 }
 
 #define retry logic
@@ -52,7 +62,7 @@ while attempts_made < 3:
     if response.status_code == 200:
         print('download successful')
         logger.info('download successful')
-        file_path = f'{data_dir}/amplitude_events_{timestamp}.zip'
+        file_path = f'{data_dir}/amplitude_events_{end_timestamp_str}.zip'
         with open(file_path, 'wb') as file:
             file.write(response.content)
         break
